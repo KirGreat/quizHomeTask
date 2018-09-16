43e7de1eceafbb4855f96e6b35d28a948f08a9c3
KirGreat <kirgreat@gmail.com>
2018-09-16 16:22:38 +0300

    updated version

--- js/moduleExample.js
+++ js/moduleExample.js
@@ -1,32 +1,53 @@
 const quiz = {
   yourAns: [],
   score: 0,
+  selectors: {
+    scoreBtn: 'check-res',
+    clearBtn: 'clear-res',
+    radioBtns: 'radio-btn'
+  },
   getAnswer(question, answer) {
     this.yourAns[question] = answer;
   },
   getScore() {
-    let answerText = '';
+    let answerText = 'Results: ';
+    const that = this;
+    questions.forEach(function(quest, i) {
+      const num = i + 1;
+      if (that.yourAns[i] != null) {
+        answerText = answerText + '\n Question#' + num + '';
+        if (that.yourAns[i] != questions[i].correctAnswer) {
+          answerText = answerText + ': Wrong ';
+        } else {
+          answerText = answerText + ': Right ';
+          that.score += 1;
+        }
+      } else {
+        answerText = answerText + '\n Question#' + num + ': No answer';
+      }
+    });
+    answerText = answerText + '\nRight answers: ' + this.score + '\n';
+    alert(answerText);
+  },
+  clear() {
+    this.yourAns = [];
+    this.score = 0;
+    document.getElementById('quiz').reset();
+  },
+  addEventListeners() {
+    const that = this;
+    document.addEventListener('click', function(e) {
+      if (e.target.classList.contains(that.selectors.scoreBtn)) {
+        that.getScore();
+      }
+      if (e.target.classList.contains(that.selectors.clearBtn)) {
+        that.clear();
+      }
+      if (e.target.classList.contains(that.selectors.radioBtns)) {
+        that.getAnswer(e.target.dataset.question, e.target.value);
+      }
+    });
   }
 };
 
-var answerText = 'Results:\n';
-for (var i = 0; i < questions.length; ++i) {
-  var num = i + 1;
-  if (yourAns[i] != null) {
-    answerText = answerText + '\n Question#' + num + '';
-    if (yourAns[i] != questions[i].correctAnswer) {
-      answerText = answerText + ': Wrong ';
-    } else {
-      answerText = answerText + ': Right ';
-      ++score;
-    }
-  } else {
-    answerText = answerText + '\n Question#' + num + ': No answer';
-  }
-}
-answerText = answerText + '\nRight answers: ' + score + '\n';
-
-alert(answerText);
-yourAns = [];
-score = 0;
-clearForm('quiz');
+quiz.addEventListeners();
